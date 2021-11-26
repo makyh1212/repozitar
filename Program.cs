ab6bee00f74ee87b7ff9f999ca083c0cd30384d6
makyh1212 <95078642+makyh1212@users.noreply.github.com>
2021-11-26 11:42:44 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void search_chars(string characters, int size)
+        {
+            int[] repetition = new int[size];
+            //initialize to 0
+            for (int i = 0; i < size; i++)
+                repetition[i] = 0;
+
+            string path = @"d:\calisma\file.txt";    
+
+            using (StreamReader sr = new StreamReader(path))
+            {
+                char[] c = null;
+                while (sr.Peek() >= 0)
+                {
+                    c = new char[1];
+
+                    sr.Read(c, 0, 1);
+
+                    // The character read is compare with
+                    // all characters searched
+                    for (int i = 0; i < size; i++)
+                    {
+                        // if he is equal he is incrementing //
+                        // the counter associated with the character
+                        if (c[0] == characters[i])
+                            repetition[i]++;
+                    }
+
+                }
+            }
+            for (int i = 0; i < size; i++)
+                Console.WriteLine("\t {0}:{1}", characters[i], repetition[i]);
+
+        }
+        static void Main(string[] args)
+        {
+
+            string str = "abcde";
+            search_chars(str, str.Length);
+
+            Console.ReadLine();
+        }
+    }
+}
